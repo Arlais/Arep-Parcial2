f966d50caf23a6417fa99d6df78be6e57f87f39a
nontoa <nicolasnc010@gmail.com>
2022-03-30 16:17:19 -0500

    final

--- src/main/java/escuelaing.arep/ProxyServer.java
+++ src/main/java/escuelaing.arep/ProxyServer.java
@@ -4,11 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
 
+import static escuelaing.arep.ProxyServer.getServidor;
 import static spark.Spark.get;
 import static spark.Spark.port;
 
 public class ProxyServer {
+    public static boolean roundRobin=true;
+    public static String[] servers={System.getenv("server1"),System.getenv("server2")};
+
     public static void main(String[] args) {
         port(getPort());
         get("/cos", (req, res) ->{
@@ -27,7 +33,7 @@ public class ProxyServer {
         return 7654; //returns default port if heroku-port isn't set (i.e. on localhost)
     }
     public static JSONObject callMethod(String value,String oper) throws IOException {
-        URL url = new URL(System.getenv("PORT")+oper+"?value="+value);
+        URL url = new URL(getServidor()+"/"+oper+"?value="+value);
         String outputLine="";
         try (BufferedReader reader= new BufferedReader(new InputStreamReader(url.openStream()))){
             String input;
@@ -38,4 +44,16 @@ public class ProxyServer {
         }
         return new JSONObject(outputLine);
     }
+
+    public static String getServidor(){
+        if(roundRobin){
+            roundRobin=!roundRobin;
+            return System.getenv("server1");
+        }
+        else{
+            roundRobin=!roundRobin;
+            return System.getenv("server1");
+        }
+    }
+
 }
